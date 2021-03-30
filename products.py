ae29069ebac3656c36fcd617417c68a55d3670d1
Dean Hsu <beatz2soul@gmail.com>
2021-03-30 17:04:11 +0800

    add for loop for printing products

--- products.py
+++ products.py
@@ -1,6 +1,6 @@
 #記帳程式
 #清單包含物件與價格
-#如何建立清單中的清單？
+#如何建立清單中的清單？二維清單
 
 products = []
 
@@ -11,3 +11,7 @@ while True:
 	price = input('請輸入商品價格:')
 	products.append([name, price]) #小清單p裝進大清單products
 print(products)
+
+#拿出大清單中的物件
+for p in products:
+	print(p[0], '的價格是', p[1])
