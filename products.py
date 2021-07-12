9cbb5cb0a1a25fa5ba0a52cb806a9132708e79de
xprssoul <79881807+xprssoul@users.noreply.github.com>
2021-07-12 21:43:32 +0800

    Add files via upload
    new file with more detailed comments

--- products.py
+++ products.py
@@ -1,36 +1,39 @@
-#記帳程式
-#清單包含物件與價格
-#如何建立清單中的清單？二維清單
-
-#先檢查要讀取的檔案在不在，不在的話讀取的時候會當掉
-import os #operating system
-#檢查檔案是否存在並讀取檔案
+"""
+這是一個記帳程式。
+"""
+import os #operating system 作業系統
+#讀取檔案
 products = []
-if os.path.isfile('products.csv'):
-	print('yeah!找到檔案了！')
-	with open('products.csv', 'r', encoding = 'utf-8') as f:
-		for line in f:
-			if '商品,價格' in line:
-				continue #繼續迴圈，跳到下一回，而break是直接跳出迴圈，會寫在比較高的位置
-			name, price = line.strip().split(',') #去掉換行符號\n，然後遇到逗號切割，切割完後變清單
-			products.append([name, price])
-	print(products)
+if os.path.isfile('products.csv'): #檢查檔案在不在
+    print('找到檔案了！')
+    with open('products.csv', 'r', encoding = 'utf-8') as f: #讀取檔案跟寫入檔案都會有編碼上的問題
+        for line in f:
+            if '商品,價格' in line:
+                continue #欄位文字不用讀取，跳過
+            name, price = line.strip().split(',') #把尾巴的換行符號拿掉、且每一行遇到逗點就分隔
+            products.append([name, price])
+    print(products)
 else:
-	print('找不到檔案.......')
-#讓使用者輸入
+    print('找不到檔案...')
+#讓使用者輸入商品名稱
 while True:
-	name = input('請輸入商品名稱:')
-	if name == 'q':
-		break
-	price = input('請輸入商品價格:')
-	price = int(price)
-	products.append([name, price]) #小清單p裝進大清單products
+    name = input('請輸入商品名稱：')
+    if name == 'q':
+        break
+    price = input('請輸入商品價格：')
+    #二維清單詳細寫法：
+        #p = []
+        #p.append(name)
+        #p.append(price)
+        #products.append(p)
+    #二維清單一行寫法：
+    products.append([name, price])
 print(products)
-#印出所有購買紀錄
+#印出所有商品價格
 for p in products:
-	print(p[0], '的價格是', p[1])
-#把使用者輸入的資料寫入檔案
-with open('products.csv', 'w', encoding = 'utf-8') as f: #這一行在打開檔案,用utf-8編碼
-	f.write('商品,價格\n')
-	for p in products:
-		f.write(p[0] + ',' + str(p[1]) + '\n') #這一行在寫入
+    print(p[0], '的價格為', p[1])
+#寫入檔案：
+with open('products.csv', 'w', encoding = 'utf-8') as f: #讀取檔案跟寫入檔案都會有編碼上的問題
+    f.write('商品,價格\n')
+    for p in products:
+        f.write(p[0] + ',' + p[1] + '\n')
